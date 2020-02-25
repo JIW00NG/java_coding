c36ac1870c9e7adcfb443efb67767ae707562955
JIW00NG <ipkjw2@gmail.com>
2020-02-25 19:13:04 +0900

    .

---
+++ Firm/src/Manager.java
@@ -0,0 +1,16 @@
+public class Manager extends Employee {
+    private String position;
+
+    public Manager(String empNo,String name,String part,String position) {
+        setEmpNo(empNo);
+        setName(name);
+        setPart(part);
+        this.position = position;
+    }
+
+    public String addStr(){
+        String result="";
+        result+="직책: "+position+"\n";
+        return result;
+    }
+}
